4d2bed519fd09e29a13e8c773957ba9ec55f7bae
kekko852 <arenafrancesco932@gmail.com>
2025-04-14 09:23:16 +0200

    Add files via upload

---
+++ ARENA_McDonalds.py
@@ -0,0 +1,37 @@
+//Arena Pio Francesco 
+print("scegli un panino/n")
+print("digita 1 per il big mac, 2 per il mc chiken,3 crispy mc bacon")
+scelta=int(input 
+if(scelta==1)//hai scelto il big mac
+spesa=spesa+7,00
+if(scelta==2)//hai scelto il mc chiken
+spesa=spesa+4,80
+if(scelta==3)//hai scelto il crispy mc bacon
+spesa=spesa+5,80
+
+print ("scegli un secondo/n")
+print("Digita 4 per le patatine, 5 per i nuggets, 6 per le alette di pollo")
+scelta=int ()
+if (scelta==1)// hai scelto le patatine
+spesa=spesa+3,00
+if(scelta==2)// hai scelto i nuggets
+spesa=spesa+4,80
+if(scelta==3)//hai scelto le alette di pollo
+spesa=spesa+3,50
+
+print ("scegli un dessert/n")
+print ("digita 7 per caffe', 8 per il mc flurry, 9 per il milk shake")
+scelta=int ()
+if (scelta==1)//hai scelto il caffe'
+spesa=spesa+1,20
+if (scelta==2)//hai scelto il mc flurry
+spesa=spesa+2,80
+if (scelta==3)//hai scelto il milk shake 
+spesa=spesa+3,50
+print("Grazie per aver scelto McDonald il totale e' :"totale)
+print("inserisci un codice sconto")
+codice=char(char('COD0102'))
+codice=='COD0102'://HAI DIRITTO A UN CODICE SCONTO
+prezzo totale=(prezzo totale"10)/100
+totale=prezzo totale
+print("Grazie hai diritto allo sconto del 10%","il totale da pagare scontato è")
